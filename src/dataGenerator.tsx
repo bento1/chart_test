1108b3650b8790a49fbe09b4d8fd7f671e5e1341
bento1 <bentotoda@naver.com>
2022-07-24 17:23:24 +0900

    hichart
    hichart

---
+++ src/dataGenerator.tsx
@@ -0,0 +1,23 @@
+type Point =[Number,Number]
+type Data = {
+    data:Point[]
+}
+type DataSet= Data[]
+
+const seriesNum:Number=2;
+const dataNum:Number=100000;
+let dataSet:DataSet=[];
+
+for (let i = 0; i < seriesNum; i++) {
+    let arr:Point[]=[]
+    for (let j = 0; j < dataNum; j++) {
+        arr.push([j,Math.random()]);
+    }
+    dataSet.push({
+        data:arr
+    });
+}
+
+
+
+export default dataSet;
---
+++ src/lineChart.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import dataSet  from './dataGenerator';
+const options = {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: 'My chart'
+    },
+    series: dataSet
+  };
+function LineChart(){
+  return(<div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+  </div>);
+}
+export default LineChart;
